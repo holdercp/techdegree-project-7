5c844fa9aca75cbe1e0a2459d37aa942a1afa68c
Chris <chris@techlinksolutions.com>
2018-08-04 13:35:11 -0400

    Convert example html to pug template

--- js/app.js
+++ js/app.js
@@ -1,6 +1,12 @@
 const express = require('express');
 
 const app = express();
-app.get('/', (req, res) => res.send('Hello, World!'));
+
+app.set('view engine', 'pug');
+app.set('views', './views');
+
+app.get('/', (req, res) => {
+  res.render('index');
+});
 
 app.listen(3000, () => console.log('The Express server is running at http://localhost:3000'));
