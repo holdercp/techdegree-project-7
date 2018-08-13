797f7b94c57a2d4cf7b5c091f1fd2e65d6dfffd2
Chris <chris@techlinksolutions.com>
2018-08-13 14:10:18 -0400

    Get profile images for DM's

--- middleware/twitter.js
+++ middleware/twitter.js
@@ -4,6 +4,15 @@ const dateHelper = require('../helpers/dates.js');
 const twitter = (config) => {
   const twit = new Twit(config);
 
+  const getSenderImg = userId => twit.get('users/show', { user_id: userId }).then(sender => sender.data.profile_image_url_https);
+
+  const processMessage = message => getSenderImg(message.message_create.sender_id).then(senderImgUrl => ({
+    senderId: message.message_create.sender_id,
+    text: message.message_create.message_data.text,
+    hoursAgo: dateHelper.hoursDiff(parseInt(message.created_timestamp, 10)),
+    senderImg: senderImgUrl,
+  }));
+
   const getTimeline = () => twit.get('statuses/home_timeline', { count: 5 }).then(result => result.data.map(tweet => ({
     name: tweet.user.name,
     handle: tweet.user.screen_name,
@@ -20,13 +29,16 @@ const twitter = (config) => {
     imageProf: friend.profile_image_url_https,
   })));
 
-  const getMessages = () => twit.get('direct_messages/events/list').then(result => result.data.events.map(message => ({
-    senderId: message.message_create.sender_id,
-    text: message.message_create.message_data.text,
-    hoursAgo: dateHelper.hoursDiff(parseInt(message.created_timestamp, 10)),
-  })));
+  const getMessages = () => twit.get('direct_messages/events/list').then((messageList) => {
+    const messageProms = messageList.data.events.map(processMessage);
+    return Promise.all(messageProms);
+  });
 
-  return { getTimeline, getFriends, getMessages };
+  return {
+    getTimeline,
+    getFriends,
+    getMessages,
+  };
 };
 
 module.exports = twitter;
